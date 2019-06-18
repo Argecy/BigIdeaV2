562fa9e9593b85aa99603f717b1202eab6d94f6b
Francois <francois.rombouts97@gmail.com>
2019-06-18 16:25:49 +0200

    Rest implemented clientside

--- websocketsclient/src/main/java/nl/fhict/s3/websocketclient/controller/LogInController.java
+++ websocketsclient/src/main/java/nl/fhict/s3/websocketclient/controller/LogInController.java
@@ -5,19 +5,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import nl.fhict.s3.restclient.UserRestClient;
+import nl.fhict.s3.restshared.User;
 
 import java.io.IOException;
 
 public class LogInController {
     @FXML
     private Button btnLogIn;
+    @FXML
+    private TextField tfUserName;
+    @FXML
+    private PasswordField tfPassword;
+    private UserRestClient userRestclient;
 
     public void initialize() {
+        userRestclient = new UserRestClient();
     }
 
     public void btnLogInClicked(ActionEvent actionEvent) {
+        User user = new User(tfUserName.getText(), tfPassword.getText());
+        if(userRestclient.authenticateUser(user) != null){
+            System.out.println("Succes");
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Check credentials");
+            alert.setHeaderText("Username or password incorrect");
+            alert.showAndWait();
+        }
     }
 
     public void btnRegisterClicked(ActionEvent actionEvent) {
